c342c37626d5a309c74ce87af94820f45673094f
lalinlulelo <lalinlulelo@gmail.com>
2019-11-05 16:15:36 +0000

    Teleport Ver1
    Only raycast pointer

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private string selectableTag = "Planet";
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                float step = Time.deltaTime; // calculate distance to move
+                var selection = hit.transform;
+                if (selection.CompareTag(selectableTag))
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, selection.position, step);
+                }
+            }
+        }       
+    }
+}
