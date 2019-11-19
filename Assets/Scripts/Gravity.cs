67949f812153805f48f5dcd07285afb2e35bb4a6
madinsane <abrownlow1@gmail.com>
2019-11-19 01:31:20 +0000

    Added 2 body physics

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Gravity body;
+    public float initialVelocity;
+    public GravityManager gravManager;
+
+    const float gravityConstant = 1;
+
+    private Vector3 gravity;
+    private Vector3 velocity;
+
+    void OnEnable()
+    {
+        gravManager.AddBody(this, rb.mass);
+    }
+
+    void OnDisable()
+    {
+        gravManager.RemoveBody(this);
+    }
+
+    void Start()
+    {
+        rb.velocity = new Vector3(initialVelocity, 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        gravity = body.GetGravity(rb.worldCenterOfMass);
+        velocity = rb.velocity;
+        velocity += gravity * Time.fixedDeltaTime;
+        rb.velocity = velocity;
+    }
+
+    public Vector3 GetGravity(Vector3 attract)
+    {
+        Vector2 delta = transform.position - attract;
+        return delta.normalized * ((rb.mass * gravityConstant) / delta.sqrMagnitude);
+    }
+}
---
+++ Assets/Scripts/GravityManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GravityManager : MonoBehaviour
+{
+    private static Dictionary<Gravity, float> bodies;
+    private static bool needsUpdate = false;
+
+    public void AddBody(Gravity body, float mass)
+    {
+        if (bodies == null)
+        {
+            bodies = new Dictionary<Gravity, float>();
+        }
+        bodies.Add(body, mass);
+        needsUpdate = true;
+    }
+
+    public void RemoveBody(Gravity body)
+    {
+        bodies.Remove(body);
+        needsUpdate = true;
+    }
+
+    private void SortBodies()
+    {
+        foreach (var item in bodies.OrderByDescending(key => key.Value))
+        {
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (needsUpdate)
+        {
+            SortBodies();
+        }
+    }
+
+}
