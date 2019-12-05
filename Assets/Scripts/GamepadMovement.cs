f9c431fede4e86256c31d02c5a0600ea80160e35
Leo-OMara <l.m.om@outlook.com>
2019-12-05 11:40:49 +0000

    both movements mostly working
    need to implement a goodway of slowing to a stop in acceleration

--- Assets/Scripts/GamepadMovement.cs
+++ Assets/Scripts/GamepadMovement.cs
@@ -7,8 +7,9 @@ public class GamepadMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
+    public Camera c;
     private Vector3 movementVector;
-    private float movementSpeed = 5.0f;
+    private float movementSpeed = 15.0f;
 
     void Start()
     {
@@ -20,11 +21,36 @@ public class GamepadMovement : MonoBehaviour
     {
         OVRInput.Update();
         movementVector = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+        // movementVector = player.transform.InverseTransformVector(movementVector);
         movementVector.x = movementVector.x * movementSpeed;
-        movementVector.y = movementVector.y * movementSpeed;
+        movementVector.z = movementVector.y * movementSpeed;
+        movementVector.y = 0;
 
+
+
+        //player.transform.position += (player.transform.forward * Time.deltaTime);
+        player.transform.rotation = c.transform.localRotation;
         player.transform.Translate(movementVector * Time.deltaTime);
 
+
+        /*
+        if (movementVector.x > 0) {
+            player.transform.localPosition += player.transform.forward * movementSpeed * Time.deltaTime;
+        }
+        if (movementVector.x < 0)
+        {
+            player.transform.localPosition += player.transform.forward * -movementSpeed * Time.deltaTime;
+        }
+        if (movementVector.y > 0) {
+            player.transform.localPosition += player.transform.right * movementSpeed * Time.deltaTime;
+        }
+        if (movementVector.y < 0)
+        {
+            player.transform.localPosition += player.transform.right * -movementSpeed * Time.deltaTime;
+        }
+
+       
+        */
 
 
         //player.transform.Translate(Vector3.right * movementVector.x * Time.deltaTime);
